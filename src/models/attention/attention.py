d6b1bd16005b43d4cab0cf0f76a5b2771598070a
ahmedelsayed968 <ahmedeltop968@gmail.com>
2024-05-07 10:39:54 +0000

    [Feat]: - Add Base class for attention modules

---
+++ src/models/attention/attention.py
@@ -0,0 +1,9 @@
+from torch.nn import Module
+
+class Attention(Module):
+    def __init__(self) -> None:
+        super().__init__()
+        # raise NotImplemented("This class must be inherited")
+    
+    def forward(self):
+        raise NotImplemented("This class must be inherited")
